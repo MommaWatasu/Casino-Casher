f9ee8d974b6b17961d3b3a3ae72ba2dbbd1bb8ca
MommawATASU <ascendwatson@gmail.com>
2023-11-04 00:17:06 +0900

    add registration system and change reservation system greatly

--- casher/src/main.rs
+++ casher/src/main.rs
@@ -1,19 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 use chrono::prelude::*;
+use chrono_tz::Asia::Tokyo;
 use json::object;
 use rocket::{get, post, routes};
 use rocket_cors::CorsOptions;
-use spin::{
-    Mutex,
-    Lazy
-};
+use spin::Mutex;
 
-const COUNT_LIMIT: u32 = 6;
-static WAIT_LIST: Mutex<Vec<User>> = Mutex::new(Vec::new());
-static RESERVE_TABLE: Lazy<Mutex<Vec<u32>>> = Lazy::new(|| Mutex::new(vec![0; 14])); 
+const COUNT_LIMIT: u32 = 2;
+// List to check waiting user
+static WAIT_LIST: Mutex<Vec<String>> = Mutex::new(Vec::new());
+// List to check reserved user
+static RESERVE_LIST: Mutex<Vec<User>> = Mutex::new(Vec::new()); 
+// Table to check count
+static RESERVE_TABLE: Mutex<[u32; 14]> = Mutex::new([0; 14]);
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Time{
     hour: usize,
     minute: usize
@@ -21,7 +23,6 @@ struct Time{
 
 impl Time {
     pub fn cmp(&self, other: Self) -> bool {
-        println!("self hour: {}, other hour: {}", self.hour, other.hour);
         if self.hour < other.hour {
             return true
         } else if self.hour == other.hour && self.minute < other.minute {
@@ -38,61 +39,91 @@ struct User {
     pub time: usize
 }
 
-#[get("/register?<time>&<ip>")]
-fn register(mut time: usize, ip: String) -> String {
-    let mut json = object!{
-        status: false,
-        err: 1
-    };
-    let now = Local::now();
-    let hour = format!("{}", now.format("%H")).parse::<usize>().unwrap();
-    let minute = format!("{}", now.format("%M")).parse::<usize>().unwrap();
+#[get("/reserve?<time>&<ip>")]
+fn reserve(mut time: usize, ip: String) -> String {
+    // status=1: default(error) code
+    let json ;
+    let now = Local::now().with_timezone(&Tokyo);
+    let mut hour = format!("{}", now.format("%H")).parse::<usize>().unwrap();
+    let mut minute = format!("{}", now.format("%M")).parse::<usize>().unwrap();
+    // test time
+    hour = 10; minute = 10;
     let now_time = Time{hour, minute};
     if time == 0 {
-        for i in 0..14 {
+        for i in 0..15 {
             if RESERVE_TABLE.lock()[i] < COUNT_LIMIT && !(Time{hour: (i+2)/2+9, minute: if (i+1)%2==1 {0} else {30}}).cmp(now_time){
                 time = i+1;
                 break
             }
         }
+        if time == 0 {
+            // status=2: time has already been full
+            json = object!{
+                status: 2
+            };
+            return json.dump();
+        }
+        println!("time: {}", time);
     } else {
         let specified_time = Time{hour: (time+1)/2+9, minute: if time%2==1 {0} else {30}};
         if specified_time.cmp(now_time) {
+            // status=3: invalid time
             json = object!{
-                status: false,
-                err: 2
+                status: 3
             };
             return json.dump()
         }
     }
-    if check_count(time) < COUNT_LIMIT {
-        let mut wait_list = WAIT_LIST.lock();
-        wait_list.push(User{ip, time});
+    if RESERVE_TABLE.lock()[time - 1] < COUNT_LIMIT {
+        let mut reserve_table = RESERVE_TABLE.lock();
+        let mut reserve_list = RESERVE_LIST.lock();
+        reserve_table[time - 1] += 1;
+        reserve_list.push(User{ ip, time });
+        // status=0: success
         json = object!{
-            status: true
+            status: 0
         };
     } else {
         json = object!{
-            status: false,
-            err: 3
+            status: 2
         };
     }
     return json.dump()
 }
 
-#[get("/get_time?<ip>")]
-fn get_time(ip: String) -> String {
-    let json = object! {
-        time: check_time(ip)
+#[post("/register?<ip>")]
+fn register(ip: String) -> String {
+    WAIT_LIST.lock().push(ip);
+    return String::from("true")
+}
+
+#[get("/check_user?<ip>")]
+fn check_user(ip: String) -> String {
+    let mut json = object! {
+        user_type: 0
     };
+    if let Some(idx) = find_reserve_ip(ip.clone()) {
+        json = object! {
+            user_type: 1,
+            time: RESERVE_LIST.lock()[idx].time
+        };
+    } else if let Some(idx) = find_wait_ip(ip) {
+        json = object! {
+            user_type: 2,
+            number: idx+1
+        }
+    }
     return json.dump()
 }
 
-#[post("/remove?<ip>")]
-fn remove(ip: String) -> String {
-    match find_ip(ip) {
+#[post("/remove_reserve?<ip>")]
+fn remove_reserve(ip: String) -> String {
+    match find_reserve_ip(ip) {
         Some(idx) => {
-            WAIT_LIST.lock().remove(idx);
+            let mut reserve_list = RESERVE_LIST.lock();
+            let time = reserve_list[idx].time;
+            reserve_list.remove(idx);
+            RESERVE_TABLE.lock()[time-1] -= 1;
             return String::from("true")
         },
         None => {
@@ -102,32 +133,42 @@ fn remove(ip: String) -> String {
     }
 }
 
-fn check_count(time: usize) -> u32 {
-    let mut count = 0;
-    let len = WAIT_LIST.lock().len();
-    for i in 0..len {
-        if WAIT_LIST.lock()[i].time == time {
-            count += 1;
+#[post("/remove_wait?<ip>")]
+fn remove_wait(ip: String) -> String {
+    match find_wait_ip(ip) {
+        Some(idx) => {
+            WAIT_LIST.lock().remove(idx);
+            return String::from("true")
+        },
+        None => {
+            return String::from("false")
         }
     }
-    return count;
 }
 
-fn check_time(ip: String) -> isize {
-    let len = WAIT_LIST.lock().len();
+#[post("/reception")]
+fn reception() -> String {
+    if WAIT_LIST.lock().pop() == None {
+        return String::from("false")
+    } else {
+        return String::from("true")
+    }
+}
+
+fn find_reserve_ip(ip: String) -> Option<usize> {
+    let len = RESERVE_LIST.lock().len();
     for i in 0..len {
-        let user = WAIT_LIST.lock()[i].clone();
-        if user.ip == ip {
-            return user.time as isize
+        if RESERVE_LIST.lock()[i].ip == ip {
+            return Some(i)
         }
     }
-    return -1;
+    return None
 }
 
-fn find_ip(ip: String) -> Option<usize> {
+fn find_wait_ip(ip: String) -> Option<usize> {
     let len = WAIT_LIST.lock().len();
     for i in 0..len {
-        if WAIT_LIST.lock()[i].ip == ip {
+        if WAIT_LIST.lock()[i] == ip {
             return Some(i)
         }
     }
@@ -136,7 +177,7 @@ fn find_ip(ip: String) -> Option<usize> {
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![register, remove, get_time])
+        .mount("/", routes![reserve, register, check_user, remove_reserve, remove_wait, reception])
         .attach(CorsOptions::default().to_cors().expect("error"))
         .launch();
 } 
--- docs/reserve.js
+++ docs/reserve.js
@@ -1,12 +1,12 @@
-const backend_url = "https://casher-yy7tpgmgqa-uw.a.run.app"
-
+// const backend_url = "https://casher-yy7tpgmgqa-uw.a.run.app"
+const backend_url = "http://localhost:8000"
 var times = new Array("9:30~10:00", "10:00~10:30", "10:30~11:00", "11:00~11:30", "11:30~12:00", "12:00~12:30", "12:30~13:00", "13:00~13:30", "13:30~14:00", "14:00~14:30", "14:30~15:00", "15:00~15:30", "15:30~16:00", "16:00~16:30")
 let ip = "";
 
 function reserve() {
   let time = document.getElementById("time-select").value;
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", backend_url+"/register?time="+String(time)+"&ip="+String(ip));
+  xhr.open("GET", backend_url+"/reserve?time="+String(time)+"&ip="+String(ip));
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
@@ -16,11 +16,11 @@ function reserve() {
         alert("予約できました");
         window.location.reload();
       } else if (data.status == 1) {
-        alert("予約に失敗しました\n無効な時間です");
+        alert("予約に失敗しました");
       } else if (data.status == 2) {
         alert("予約に失敗しました\n定員オーバーなので、他の時間を選択してください")
       } else {
-        alert("予約に失敗しました\nすでに予約されています");
+        alert("予約に失敗しました\n無効な時間です");
       }
     } else {
       alert(`予約に失敗しました: ${xhr.status}`);
@@ -28,9 +28,42 @@ function reserve() {
   };
 }
 
-function cancel() {
+function register() {
   const xhr = new XMLHttpRequest();
-  xhr.open("POST", backend_url+"/remove?ip="+String(ip));
+  xhr.open("POST", backend_url+"/register?ip="+String(ip));
+  xhr.send();
+  xhr.responseType = "text";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      alert("整理券を発行しました");
+      window.location.reload();
+    }
+  }
+}
+
+function cancel_wait() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", backend_url+"/remove_wait?ip="+String(ip));
+  xhr.send();
+  xhr.responseType = "text";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      const data = xhr.response;
+      if (data == "true") {
+        alert("キャンセルしました");
+        window.location.reload();
+      } else {
+        alert("キャンセルに失敗しました\n無効なIPアドレスです")
+      }
+    } else {
+      alert(`キャンセルに失敗しました: ${xhr.status}`);
+    }
+  }
+}
+
+function cancel_reserve() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", backend_url+"/remove_reserve?ip="+String(ip));
   xhr.send();
   xhr.responseType = "text";
   xhr.onload = () => {
@@ -58,15 +91,19 @@ window.addEventListener("load", async function() {
       }
     });
   const xhr = new XMLHttpRequest();
-  xhr.open("GET", backend_url+"/get_time?ip="+String(ip));
+  xhr.open("GET", backend_url+"/check_user?ip="+String(ip));
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       const data = xhr.response;
-      if (data.time != -1) {
-        document.getElementById("reserve").innerHTML = `<p id="result">${times[data.time-1]}</p><br><p>予約をキャンセルする場合は以下のボタンを押してください</p><div class="button" onclick="cancel()">キャンセル</div>`;
+      if (data.user_type == 1) {
+        document.getElementById("reserve").innerHTML = `<p id="result">${times[data.time-1]}</p><br><p>予約をキャンセルする場合は以下のボタンを押してください</p><div class="button" onclick="cancel_reserve()">キャンセル</div>`;
+        document.getElementById("register").innerHTML = ""; 
+      } else if (data.user_type == 2) {
+        document.getElementById("reserve").innerHTML = "";
+        document.getElementById("register").innerHTML = `<p id=result>${data.number}番目</p><br><p>整理券をキャンセルする場合は以下のボタンを押してください</p><div class="button" onclick="cancel_wait()">キャンセル</div>`;
       }
-    } else {}
+    }
   };
 });
